89d8e7097612305bb73f89473dcd05d7e147ece8
Francisco Assis Malachias de Almeida <127680274+AssisMalachias@users.noreply.github.com>
2025-07-26 16:12:44 -0300

    criar do  estrução imagems

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,27 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const debug = require('gulp-debug');
+const imagemin = require('gulp-imagemin');
 
-
-function Text(cb){
-    console.log('Hello World!');
-    cb();
+function style() {
+    return gulp.src('./src/css/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = Text;
+function images() {
+    return gulp.src('./src/image/**/*.+(png|jpg|jpeg|gif|svg)')
+    .pipe(debug({title:'imagem encontrada:'}))
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/image'));
+}
+
+
+function watch(){
+    gulp.watch('./src/css/*.scss',style);
+}
+exports.default = gulp.series(
+    gulp.parallel(style, images),
+    watch
+);
+
