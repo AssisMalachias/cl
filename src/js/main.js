6a0bcf9ef8241a22faa5545f36c8cf337be1d822
Francisco Assis Malachias de Almeida <127680274+AssisMalachias@users.noreply.github.com>
2025-07-29 21:30:02 -0300

    criar rodaper

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
     
 
     for(let i = 0; i < buttons.length;i++){
@@ -13,8 +14,20 @@ document.addEventListener('DOMContentLoaded', function() {
             
         })
     }
+
+    for(let i = 0; i < questions.length;i++){
+        questions[i].addEventListener('click',abreOuFechaResposta)
+    }
    
 });
+
+function abreOuFechaResposta(elemento) {
+     const classe = 'faq__questions__item--is-open';
+     const elementoPai =  elemento.target.parentNode;
+
+     elementoPai.classList.toggle(classe);
+}
+
 function removeBotaoAltivo(){
      const buttons = document.querySelectorAll('[data-tab-button]');
 
